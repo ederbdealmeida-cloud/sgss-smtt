f971e84ade96754c118120c33ca8d41ebdc2ba18
SGSS Claude <sgss@smtt.cacequi>
2026-05-23 16:31:22 +0000

    fix: APP_VER 5.4.10.0 + SW v11 forca reload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sgss-v10';
+const CACHE = 'sgss-v11';
 const ASSETS = ['/sgss-smtt/','/sgss-smtt/index.html','/sgss-smtt/icon-192.png','/sgss-smtt/icon-512.png','/sgss-smtt/manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
